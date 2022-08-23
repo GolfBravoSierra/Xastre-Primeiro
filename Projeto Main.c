5fb6d94a67b46ea9e9f27513343c263fbd00c5ee
Jean Piton <55934276+jean33487@users.noreply.github.com>
2022-08-23 10:58:58 -0300

    Quick sort
    copiei e colei o quick sort de https://pt.wikipedia.org/wiki/Quicksort mas esta em c++

--- Projeto Main.c
+++ Projeto Main.c
@@ -2,11 +2,40 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-
+void quicksort(int values[], int began, int end)
+{
+	int i, j, pivo, aux;
+	i = began;
+	j = end-1;
+	pivo = values[(began + end) / 2];
+	while(i <= j)
+	{
+		while(values[i] < pivo && i < end)
+		{
+			i++;
+		}
+		while(values[j] > pivo && j > began)
+		{
+			j--;
+		}
+		if(i <= j)
+		{
+			aux = values[i];
+			values[i] = values[j];
+			values[j] = aux;
+			i++;
+			j--;
+		}
+	}
+	if(j > began)
+		quicksort(values, began, j+1);
+	if(i < end)
+		quicksort(values, i, end);
+}
 
 int main() 
 {
-printf("com vc esta\n");
+  
 
 return 0;
 }
