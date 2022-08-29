abc0f43d97d2c8931021e03bb30f0bde27a8be1a
MatheusZCAR <matheuscarita@gmail.com>
2022-08-29 16:30:22 -0300

    SolucaoReal
    Acho que foi

--- Projeto Main.c
+++ Projeto Main.c
@@ -1,13 +1,121 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdio.h>
+#include <time.h>
 
-
-
-int main() 
+///funcao que faz um quickShort
+void quicksort(int values[], int began, int end)
 {
-printf("com vc esta\n");
-printf("isso eh um teste");
+	int i, j, pivo, aux;
+	i = began;
+	j = end-1;
+	pivo = values[(began + end) / 2];
+	while(i <= j)
+	{
+		while(values[i] < pivo && i < end)
+		{
+			i++;
+		}
+		while(values[j] > pivo && j > began)
+		{
+			j--;
+		}
+		if(i <= j)
+		{
+			aux = values[i];
+			values[i] = values[j];
+			values[j] = aux;
+			i++;
+			j--;
+		}
+	}
+	if(j > began)
+		quicksort(values, began, j+1);
+	if(i < end)
+		quicksort(values, i, end);
+}
 
-return 0;
+///funcao que faz a media do vetor
+float avarage(int *vet)
+{
+    float media;
+    for(int i = 0; i<10;i++)// mudei aqui
+    {
+        media += vet[i];
+    }
+    media /= 2;
+    return media;
+}
+
+///funcao de swap
+void swap(int a, int b){
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+///funcao que faz um bobbleShort
+void bubbleSort(int v[], int n){
+    if (n < 1)return;
+
+    for (int i=0; i<n; i++)
+        if (v[i] > v[i+1])
+            swap(v[i], v[i+1]);
+    bubbleSort(v, n-1);
+}
+
+
+int main()
+{
+    srand(time(NULL));//muda a seed do codigo para deixar a rand aleatoria de a cordo com o horario atual
+    //iniciando variaveis
+    int n=1000;
+    int ar[1000]={};
+    int ar2[1000]={};
+    float media;//media do vetor
+    int soma=0;// soma para a media do vetor
+
+    //alocando 1000 numeros aleatorios no vetor
+    for(int i=0; i<n; i++){
+    ar[i]=rand()%1000;
+    ar2[i]=ar[i];
+
+  }
+
+
+    //printa o array original
+    printf("Array Original:\n");
+    for(int i=0; i<n; i++){
+        printf("%i ",ar[i]);
+        
+    }
+
+    //chama funcao quickShort
+    quicksort(ar2, 0, n);
+
+    //printa o array do quickshort
+    printf("\n\nQuickSort:\n");
+    for(int i=0; i<n; i++){
+        printf("%i ",ar2[i]);
+    }
+
+    //chama a funcao bobbleshort
+    bubbleSort(ar2, n);
+
+    //printa o array do bobbleShort
+    printf("\n\nBubbleSort:\n");
+    for(int i=0; i<n; i++){
+        printf("%i ",ar2[i]);
+    }
+  for(int i =0; i<1000; i++){// esse for é para faser a media do vetor
+    soma = soma + ar[i];
+    //printf("soma para verificar a media\n%d\n", soma);
+  }
+  media=soma/1000;
+    printf("\n\nMediana do Array Inicial: %i\n\n", (ar2[500]+ar2[501])/2);// printa a mediana do array inicial
+    printf("\n\nMediana do BubbleSort: %i\n\n", (ar2[500]+ar2[501])/2);// printa a mediana do array do bobbleshort
+    printf("\n\nMediana do QuickSort: %i\n\n", (ar2[500]+ar2[501])/2);
+    printf("\n\nMedia: %.2f\n\n", media);//printa a media do array
+
+    
+    return 0;
 }
